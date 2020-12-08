fe2912380cbd9af12f8b1c9d906ff469ed8c4fe5
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-08 12:20:23 -0500

    Correction an l

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,7 +3,7 @@ def fizzbuzz():
     for i in range(101):
         if i % 3 == 0 and i % 5 == 0:
             print("FizzBuzz", end=" ")
-        lif i % 3 == 0:
+        if i % 3 == 0:
             print("Fizz", end=" ")
         elif i % 5 == 0:
             print("Buzz", end=" ")
