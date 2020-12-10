7d45a0f620b55f82e317027f55dfa7f09ec6ef61
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-10 10:18:31 -0500

    Create second file

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    l = len(argv) - 1
+
+    if l == 0:
+        print("0 arguments.")
+    elif l == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(l))
+    for i in range(1, len(argv)):
+        print("{}".format(argv[i]))
+
+
