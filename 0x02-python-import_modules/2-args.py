0bab1b028f517d3c7666722844f15ac48d76fd1f
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-10 12:01:55 -0500

    Add index in the print

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,4 +11,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(l))
     for i in range(1, len(argv)):
-        print("{}".format(argv[i]))
+        print("{}: {}".format(i, argv[i]))
