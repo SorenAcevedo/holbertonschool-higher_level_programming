33e480a0b14350b91cae5bac50e05d565c493440
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-15 12:32:14 -0500

    Add none condition

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    a = (len(sentence), sentence[0])
+    if sentence:
+        a = (len(sentence), sentence[0])
+    else:
+        a = (0, None)
     return a
