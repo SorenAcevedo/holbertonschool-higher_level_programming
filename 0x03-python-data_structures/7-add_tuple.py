04cdf87c8c900370d451da9e1d55623144b11936
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-15 11:36:46 -0500

    Correction 07 with lists

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = list(tuple_a)
-    b = list(tuple_b)
-    for i in range(0, 2):
-        if not a[i]:
-            a.append(0)
-        if not b[i]:
-            b.append(0)
+    a = [0, 0]
+    b = [0, 0]
+    for i in range(len(tuple_a)):
+        a[i] = tuple_a[i]
+    for i in range(len(tuple_b)):
+        b[i] = tuple_b[i]
     c = (a[0] + b[0], a[1] + b[1])
     return c
