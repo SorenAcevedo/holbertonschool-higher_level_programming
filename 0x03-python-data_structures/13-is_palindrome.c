e671ebd81cb4e2642c47004ce5b0c8bb2b70dd35
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-15 16:19:33 -0500

    Correction

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,24 @@
 #include "lists.h"
 
 /**
+ * len_list - calculate len of list
+ * @head: head
+ * Return: len
+ */
+int len_list(listint_t **head)
+{
+	listint_t *temp = *head;
+	int i = 0;
+
+	while (temp)
+	{
+		temp = temp->next;
+		i++;
+	}
+	return (i);
+}
+
+/**
  * is_palindrome - check if a list is palindrome
  * @head: pointer to head
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -11,17 +29,20 @@ int is_palindrome(listint_t **head)
 	int i = 0, j = 0, k;
 	int *arr;
 
-	if (*head == NULL)
+
+	i = len_list(head);
+	if (i == 0 || i == 1)
 		return (1);
-	arr = malloc(sizeof(int) * 1024);
+	arr = malloc(sizeof(int) * i);
 
 	while (temp)
 	{
-		arr[j] = temp->n;
+		if (j < i)
+			arr[j] = temp->n;
 		temp = temp->next;
 		j++;
 	}
-	k = j--;
+	j--;
 	for (i = 0; j > k / 2; i++, j--)
 	{
 		if (arr[i] != arr[j])
