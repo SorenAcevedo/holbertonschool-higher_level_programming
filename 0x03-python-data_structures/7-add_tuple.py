1a85134eca4c55d1cf0f2b3a77a5d605449eed50
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-15 12:39:50 -0500

    Range correction

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,9 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = [0, 0]
     b = [0, 0]
-    for i in range(len(tuple_a)):
+    for i in range(len(tuple_a), 2):
         a[i] = tuple_a[i]
-    for i in range(len(tuple_b)):
+    for i in range(len(tuple_b), 2):
         b[i] = tuple_b[i]
     c = (a[0] + b[0], a[1] + b[1])
     return c
