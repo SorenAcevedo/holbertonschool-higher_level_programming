8e42b283290bb7c8b2765900ef5143782c6d8865
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-15 12:44:10 -0500

    Correction when tuple is longer

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,9 +2,18 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = [0, 0]
     b = [0, 0]
-    for i in range(len(tuple_a), 2):
+
+    l1 = len(tuple_a)
+    if l1 >= 2:
+        l1 = 2
+    l2 = len(tuple_b)
+    if l2 >= 2:
+        l2 = 2
+
+    for i in range(l1):
         a[i] = tuple_a[i]
-    for i in range(len(tuple_b), 2):
+    for i in range(l2):
         b[i] = tuple_b[i]
+
     c = (a[0] + b[0], a[1] + b[1])
     return c
