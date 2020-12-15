6a686df27a4b30ad75eb430b9a22a5ff427b6d9b
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-15 16:34:10 -0500

    Create buffer

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,24 +1,6 @@
 #include "lists.h"
 
 /**
- * len_list - calculate len of list
- * @head: head
- * Return: len
- */
-int len_list(listint_t **head)
-{
-	listint_t *temp = *head;
-	int i = 0;
-
-	while (temp)
-	{
-		temp = temp->next;
-		i++;
-	}
-	return (i);
-}
-
-/**
  * is_palindrome - check if a list is palindrome
  * @head: pointer to head
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -26,19 +8,14 @@ int len_list(listint_t **head)
 int is_palindrome(listint_t **head)
 {
 	listint_t *temp = *head;
-	int i = 0, j = 0, k;
-	int *arr;
+	int i, j = 0, k;
+	int arr[24];
 
-
-	i = len_list(head);
-	if (i == 0 || i == 1)
+	if (!*head)
 		return (1);
-	arr = malloc(sizeof(int) * i);
-
 	while (temp)
 	{
-		if (j < i)
-			arr[j] = temp->n;
+		arr[j] = temp->n;
 		temp = temp->next;
 		j++;
 	}
@@ -46,11 +23,7 @@ int is_palindrome(listint_t **head)
 	for (i = 0; j > k / 2; i++, j--)
 	{
 		if (arr[i] != arr[j])
-		{
-			free(arr);
 			return (0);
-		}
 	}
-	free(arr);
 	return (1);
 }
