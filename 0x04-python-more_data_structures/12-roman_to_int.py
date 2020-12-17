9d94a5e44774894345de881e4781d8253d9012b3
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-17 17:12:50 -0500

    Create 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    rs = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    num = 0
+    if roman_string:
+        rs = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        num = 0
 
-    for i in range(len(roman_string)):
-        if i < len(roman_string) - 1:
-            if rs.get(roman_string[i]) < rs.get(roman_string[i + 1]):
-                num += rs.get(roman_string[i + 1]) - 1
-                if i == len(roman_string) - 2:
-                    break
+        for i in range(len(roman_string)):
+            if i < len(roman_string) - 1:
+                if rs.get(roman_string[i]) < rs.get(roman_string[i + 1]):
+                    num += rs.get(roman_string[i + 1]) - 1
+                    if i == len(roman_string) - 2:
+                        break
+                else:
+                    num += rs.get(roman_string[i])
             else:
                 num += rs.get(roman_string[i])
-        else:
-            num += rs.get(roman_string[i])
-    return num
+        return num
+    return 0
