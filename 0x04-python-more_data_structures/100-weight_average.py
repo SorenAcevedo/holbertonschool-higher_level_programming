af54906b60a173039f1998ff7cf42b718bab69ae
SorenAcevedo <sorenacevedo@gmail.com>
2020-12-17 15:18:18 -0500

    Create 13

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def mul(t=()):
+    c = 1
+    for i in t:
+        c *= i
+    return c
+
+
+def weight_average(my_list=[]):
+    return sum([mul(i) for i in my_list]) / sum([i[1] for i in my_list])
