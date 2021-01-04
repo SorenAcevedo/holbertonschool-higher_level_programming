8f7c7880047ad3c58e3620814386b285b1a986bc
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-04 12:30:24 -0500

    Change for cycle

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     c = 0
-    for i in my_list:
+    for i in range(x):
         try:
             if c < x:
-                print("{:d}".format(i), end='')
+                print("{:d}".format(my_list[i]), end='')
                 c += 1
         except:
             continue
