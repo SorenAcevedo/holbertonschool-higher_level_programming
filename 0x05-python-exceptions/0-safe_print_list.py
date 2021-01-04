714ca594d09d255fce9b7eafa203715c97f9edb1
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-04 12:27:52 -0500

    Correction, change for by while

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        flag = 1
-        for i, j in enumerate(my_list):
-            if i >= x:
-                flag = 0
-                break
-            print(j, end='')
+        i = 0
+        while(i < x):
+            print(my_list[i], end='')
+            i += 1
         print()
-        return i + flag
+        return i
     except:
         print()
-        return i + flag
+        return i
