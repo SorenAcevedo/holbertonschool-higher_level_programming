3628e4791d5c5dc6db75dde920410d76372f4fe2
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-04 11:25:44 -0500

    Create first

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        flag = 1
+        for i, j in enumerate(my_list):
+            if i >= x:
+                flag = 0
+                break
+            print(j, end='')
+        print()
+        return i + flag
+    except:
+        print("Error")
