93e40d78407d79c9e3c0851a11867f66291b106c
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-05 15:45:42 -0500

    Add comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+This module have de square Class
+"""
 
 
 class Square:
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,40 +1,91 @@
 #!/usr/bin/python3
+"""
+This module have de square Class
+"""
 
 
 class Square:
     """ Class Square """
-    def __init__(self, __size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """ Initializated method
         Args:
-            __size (int): Size of square.
+            size (int): Size of square.
+            position (int tuple):
         Attributes:
             __size (int): Size of square.
         """
-        self.size = __size
-        self.__size = self.size
-        
-        if type(self.size) != int:
+
+        if type(size) != int:
             raise TypeError('size must be an integer')
-        if __size < 0:
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
+
+        if (type(position) != tuple or
+                len(position) != 2 or
+                type(position[0]) != int or
+                type(position[1]) != int or
+                position[0] < 0 or
+                position[1] < 0):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = position
+
+    @property
+    def size(self):
+        """ Method to retrieve size """
+
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ Method to set new size
+        Args:
+            self: instance
+            value (int): new size
+        """
+
+        if type(value) != int:
+            raise TypeError('size must be an integer')
+
+        elif value < 0:
             raise ValueError('size must be >= 0')
 
-    def area(self):
-        """ Area public method
+        self.__size = value
+
+    @property
+    def position(self):
+        """ Method to retrieve position """
+
+        return self.__position
+
+    @size.setter
+    def position(self, value):
+        """ Method to set new position
         Args:
-            self: isntance
+            value (int): new position
         """
-        if type(self.size) != int:
+
+        if (type(position) != tuple or
+                len(position) != 2 or
+                type(position[0]) != int or
+                type(position[1]) != int or
+                position[0] < 0 or
+                position[1] < 0):
+            raise TypeError('position must be a tuple of 2 positive integers')
+        self.__position = value
+
+    def area(self):
+        """ Area public method """
+
+        if type(self.__size) != int:
             raise TypeError('size must be an integer')
-        return self.size ** 2
+        return self.__size ** 2
 
     def my_print(self):
-        """ Print square public method
-            Args:
-                self: isntance
-        """
-        if self.size > 0:
-            for i in range(self.size):
-                print("#", end="")
-        else:
+        """ Print square method """
+        if self.__size < 0:
             print()
+            return
 
+        for i in range(self.__size):
+            print("{}".format((" " * self.__position[0] + "#" * self.__size)))
