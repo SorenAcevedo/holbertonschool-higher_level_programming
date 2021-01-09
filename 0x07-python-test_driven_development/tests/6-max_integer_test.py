7958e0e7dec0d36dd422a0b280207698ed4bf6f2
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-08 19:47:47 -0500

    Pep 8 style

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -4,21 +4,22 @@
 import unittest
 max_integer = __import__('6-max_integer').max_integer
 
+
 class TestMaxInteger(unittest.TestCase):
-	""" Class with test """
-	def test_max(self):
-		""" Tests
-			1. Ordered list
-			2. Not ordered list
-			3. Negative list
-			4. Equal elements list
-			5. None Case
-		"""
-		self.assertEqual(max_integer([1, 2, 3, 4]), 4)
-		self.assertEqual(max_integer([1, 2, 4, 3]), 4)
-		self.assertEqual(max_integer([-1, -2, -4, -3]), -1)
-		self.assertEqual(max_integer([4, 4, 4, 4]), 4)
-		self.assertEqual(max_integer([]), None)
+    """ Class with test """
+    def test_max(self):
+	""" Tests
+        1. Ordered list
+        2. Not ordered list
+        3. Negative list
+        4. Equal elements list
+        5. None Case
+        """
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+        self.assertEqual(max_integer([1, 2, 4, 3]), 4)
+        self.assertEqual(max_integer([-1, -2, -4, -3]), -1)
+        self.assertEqual(max_integer([4, 4, 4, 4]), 4)
+        self.assertEqual(max_integer([]), None)
 
 if __name__ == '__main__':
     unittest.main()
