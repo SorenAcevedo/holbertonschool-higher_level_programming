4637d6e1e9dda5783ca3f936a456d863391d4f34
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-11 15:02:37 -0500

    Correction

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -49,3 +49,8 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return 0
         return 2 * self.__width + 2 * self.__height
+
+    def __str__(self):
+        """ Method to represent instance """
+        l = self.__height
+        return "".join([('#' * self.__width) + '\n' for i in range(l)])[:-1]
