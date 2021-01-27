a9afec50d1960a853834f6f4400b5c60a9cd8121
SorenAcevedo <sorenacevedo@gmail.com>
2021-01-27 10:17:03 -0500

    Correction 11

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -9,22 +9,17 @@ class Square(Rectangle):
         """ Initializated method """
         super().__init__(size, size, x, y, id)
         self.size = size
-        self.x = x
-        self.y = y
 
     @property
     def size(self):
         """ Method to retrieve height """
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         """ Method to set height """
-        if type(value) != int:
-            raise TypeError('width must be an integer')
-        if value <= 0:
-            raise ValueError('width must be > 0')
-        self.__size = value
+        self.width = value
+        self.height = value
 
     def update(self, *args, **kwargs):
         """ update method """
