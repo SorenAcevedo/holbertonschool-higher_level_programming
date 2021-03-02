acb1dc835d506d8a0e9f1d01bd482e47ab61e570
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-02 15:45:24 -0500

    Add 1

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- Script that creates the MySQL server user user_0d_1
--- All privileges on your MySQL server
-CREATE USER 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost';
