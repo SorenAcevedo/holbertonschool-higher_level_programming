35697a7832e7ef17bd05b60768a3eb1c7b9e3c7b
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-02 21:59:30 -0500

    Add 14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -3,6 +3,6 @@
 SELECT tv_genres.name
 FROM tv_genres
 INNER JOIN tv_show_genres ON tv_show_genres.genre_id = tv_genres.id
-INNER JOIN tv_show ON tv_show_genres.show_id = tv_show.id
-WHERE tv_show.name = "Dexter"
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
 ORDER BY tv_genres.name;
