de67d20de4b662e044e1f3162ddb4dbf88451940
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-22 15:51:14 -0500

    Add more thant 1 argument case

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,9 @@
 #!/usr/bin/node
 const len = process.argv.length;
-if (len > 2) {
+if (len === 3) {
   console.log('Argument found');
+} else if (len > 3) {
+  console.log('Arguments found');
 } else {
   console.log('No argument');
 }
