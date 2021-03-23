bcb97b10a7b8e45223940b8626c021da945d723a
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-22 21:57:24 -0500

    Correction

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,14 @@
 #!/usr/bin/node
 const len = process.argv.length;
-const arr = process.argv.slice(2, len).sort();
+const max = Math.max(...process.argv.slice(2, len));
+let second = Math.min(...process.argv.slice(2, len));
 if (len <= 3) {
   console.log(0);
 } else {
-  console.log(arr[len - 4]);
+  for (let i = 2; i < len; i++) {
+    if (parseInt(process.argv[i]) >= second && parseInt(process.argv[i]) < max) {
+      second = parseInt(process.argv[i]);
+    }
+  }
+  console.log(second);
 }
