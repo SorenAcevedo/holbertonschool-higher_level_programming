53dd1fbfc584e6ab3817c3995b15724323f0656a
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-23 16:23:43 -0500

    Add 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const S2 = require('./5-square');
+
+class Square extends S2 {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c !== undefined) {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Square;
