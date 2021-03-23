f06723516794041cc513cc17e3c0f71218bd8c6d
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-22 20:15:33 -0500

    Add 7, condition and loops

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const first = process.argv[2];
+let i;
+if (isNaN(parseInt(first)) || first === undefined) {
+  console.log('Missing number of occurrences');
+} else {
+  for (i = 0; i < process.argv[2]; i++) {
+    console.log('C is fun');
+  }
+}
