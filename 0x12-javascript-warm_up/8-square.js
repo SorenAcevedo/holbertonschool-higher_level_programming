04526aafeb8c3d9a98cbde7d229dad86b01df89d
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-22 20:26:06 -0500

    Add 8, use repeat

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,10 @@
 #!/usr/bin/node
 const first = process.argv[2];
-if (isNaN(first)) {
-  console.log('Missing number of occurrences');
+let i;
+if (isNaN(parseInt(first)) || first === undefined) {
+  console.log('Missing size');
 } else {
-  for (let i = 0; i < first; i++) {
+  for (i = 0; i < first; i++) {
     console.log('X'.repeat(first));
   }
 }
