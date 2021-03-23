f0cdc744ee3a6e5f52d970274b5d50eb940fe7e5
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-22 21:18:22 -0500

    Add 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const len = process.argv.length;
+const max = Math.max(...process.argv.slice(2, len - 1));
+let second = Math.min(...process.argv.slice(2, len - 1));
+if (len === 2) {
+  console.log(1);
+} else {
+  for (let i = 2; i < len; i++) {
+    if (process.argv[i] > second && process.argv[i] < max) {
+      console.log(second);
+      second = process.argv[i];
+    }
+  }
+  console.log(second);
+}
