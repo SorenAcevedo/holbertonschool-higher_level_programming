011a87b638f598c158a8b727fd48ea18e6250c23
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-27 17:43:11 -0500

    Change some error

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,14 +13,16 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://' +
                            '{}:{}@localhost/{}'.format(sys.argv[1],
-                                                       "soren",
+                                                       sys.argv[2],
                                                        sys.argv[3]),
                            pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(engine)
     session = Session()
 
     sts_cts = session.query(State, City).filter(
-        State.id == City.state_id).order_by(City.id)
+        State.id == City.state_id).order_by(City.id).all()
     for s, c in sts_cts:
         print("{}: ({}) {}".format(s.name, c.id, c.name))
+    session.close()
