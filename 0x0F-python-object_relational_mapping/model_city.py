ff79ad883ecd18de83fba6887d312a19bb06e3cd
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-27 17:30:38 -0500

    Impoort from old base

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -5,9 +5,7 @@ of a State and an instance
 Base = declarative_base()
 """
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-
-Base = declarative_base()
+from model_state import Base
 
 
 class City(Base):
