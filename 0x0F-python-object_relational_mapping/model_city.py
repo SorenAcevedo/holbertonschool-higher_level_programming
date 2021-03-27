03912dbf9f37b1f2cfb66271052f49cf1d60e4bb
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-27 17:33:01 -0500

    Add comment

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -9,6 +9,7 @@ from model_state import Base
 
 
 class City(Base):
+    """ City Class/Model inherit from Base"""
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True,
                 unique=True,
