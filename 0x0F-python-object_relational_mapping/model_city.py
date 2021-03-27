e6086d3f8551fef35da1e040c277b5dc50b039c2
SorenAcevedo <sorenacevedo@gmail.com>
2021-03-27 11:54:13 -0500

    Add model city

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module contains the class definition
+of a State and an instance
+Base = declarative_base()
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    __tablename__ = "cities"
+    id = Column(Integer, primary_key=True,
+                unique=True,
+                autoincrement=True,
+                nullable=False)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, nullable=False,  ForeignKey('states.id'))
