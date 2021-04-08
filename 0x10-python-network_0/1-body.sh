2f2402b965e7bbdee2aeebdbcbf25a416cf51521
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-08 17:53:59 -0500

    Add 0-2 points

---
+++ 0x10-python-network_0/1-body.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Script that sends a get Http request
+# and displays the body
+curl "$1"
---
+++ 0x10-python-network_0/2-delete.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Script that sends a delete Http request
+# and displays the body
+curl "$1" -X DELETE
