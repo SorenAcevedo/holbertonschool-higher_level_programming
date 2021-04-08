cc454882f9ec67acddbf5c61522e7c42a61ddba1
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-08 17:59:52 -0500

    Delete 1 line comment for the requirementes

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that sends a get Http request
-# and displays the body
 curl -sL "$1"
--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that sends a delete Http request
-# and displays the body
 curl -sL "$1" -X DELETE
