7785c35f3a3ffe7e3c7d771f9880db81111105a4
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-09 13:33:27 -0500

    Add 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,10 +3,11 @@ import urllib.request
 import urllib.parse
 import sys
 
-if len(sys.argv) == 3:
-	url = sys.argv[1]
-	values = {'email': sys.argv[2]}
-	data = urllib.parse.urlencode(values)
-	data = data.encode('ascii')
-    with urllib.request.urlopen(url, data) as response:
-        print(response.read())
+if __name__ == "__main__":
+	if len(sys.argv) == 3:
+		url = sys.argv[1]
+		values = {'email': sys.argv[2]}
+		data = urllib.parse.urlencode(values)
+		data = data.encode('ascii')
+		with urllib.request.urlopen(url, data) as response:
+			print(response.read())
