8502b462b9ac8af5a53acc49425fd236465dcb28
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-09 16:09:52 -0500

    Add 8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -4,17 +4,17 @@ import requests
 import sys
 
 if __name__ == "__main__":
-	values = {'p': ''}
-	if len(sys.argv) == 2:
-		values['p'] = sys.argv[1]
-	url = 'http://0.0.0.0:5000/search_user'
-	r = requests.post(url, data=values)
-	print(r.headers)
-	try:
-		r_json = r.json()
-		if r_json:
-			print('[{}] {}'.format(r_json['id'], r_json['name']))
-		else:
-			print('No result')
-	except:
-		print('Not a valid JSON')
+    values = {'p': ''}
+    if len(sys.argv) == 2:
+        values['p'] = sys.argv[1]
+    url = 'http://0.0.0.0:5000/search_user'
+    r = requests.post(url, data=values)
+    print(r.headers)
+    try:
+        r_json = r.json()
+        if r_json:
+            print('[{}] {}'.format(r_json['id'], r_json['name']))
+        else:
+            print('No result')
+    except:
+        print('Not a valid JSON')
