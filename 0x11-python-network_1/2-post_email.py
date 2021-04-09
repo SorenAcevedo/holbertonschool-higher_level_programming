ae390c8839d0f3ee0f9a5f009f9c19ba149d201a
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-09 13:35:00 -0500

    Add 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,13 +1,14 @@
+#!/usr/bin/python3
 """ This module fetches url """
 import urllib.request
 import urllib.parse
 import sys
 
 if __name__ == "__main__":
-	if len(sys.argv) == 3:
-		url = sys.argv[1]
-		values = {'email': sys.argv[2]}
-		data = urllib.parse.urlencode(values)
-		data = data.encode('ascii')
-		with urllib.request.urlopen(url, data) as response:
-			print(response.read())
+    if len(sys.argv) == 3:
+        url = sys.argv[1]
+        values = {'email': sys.argv[2]}
+        data = urllib.parse.urlencode(values)
+        data = data.encode('ascii')
+        with urllib.request.urlopen(url, data) as response:
+            print(response.read())
