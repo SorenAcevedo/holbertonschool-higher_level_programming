6c589cda0a3e19e7cecdc91946866310a8a8e9bb
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-08 20:48:49 -0500

    Add 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -9,15 +9,6 @@ def find_peak(list_of_integers):
     if not list_of_integers or leng == 0:
         return None
 
-    if leng > 2:
-        peak = list_of_integers[leng // 2]
-        right = list_of_integers[(leng // 2) + 1]
-        left = list_of_integers[(leng // 2) - 1]
-        if left < peak > right:
-            return peak
-        elif left < peak:
-            return find_peak(list_of_integers[(leng // 2) + 1:])
-        else:
-            return find_peak(list_of_integers[:leng // 2])
     else:
-        return max(list_of_integers)
+        list_of_integers.sort()
+        return list_of_integers[-1]
