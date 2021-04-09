12b45155d3ba99d303f00c508c78d70de8d448c6
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-09 16:08:53 -0500

    Add 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" This module fetches url """
+import requests
+import sys
+
+if __name__ == "__main__":
+	values = {'p': ''}
+	if len(sys.argv) == 2:
+		values['p'] = sys.argv[1]
+	url = 'http://0.0.0.0:5000/search_user'
+	r = requests.post(url, data=values)
+	print(r.headers)
+	try:
+		r_json = r.json()
+		if r_json:
+			print('[{}] {}'.format(r_json['id'], r_json['name']))
+		else:
+			print('No result')
+	except:
+		print('Not a valid JSON')
