19f350b0d05227be65a3135e093a7dd19b691013
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-09 14:32:06 -0500

    Using requests package

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """ This module fetches url """
-import urllib.request
+import requests
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-        html = response.read()
-        print('Body response:')
-        print('\t- type: {}'.format(type(html.decode('utf-8'))))
-        print('\t- content: {}'.format(html.decode('utf-8')))
+	r = requests.get('https://intranet.hbtn.io/status')
+	print('Body response:')
+	print('\t- type: {}'.format(type(r.text)))
+	print('\t- content: {}'.format(r.text))
