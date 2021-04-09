2188aba104546c5ed1f94868ad667384a6858e61
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-09 12:35:41 -0500

    Add imported validation

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,10 +2,10 @@
 """ This module fetches url """
 import urllib.request
 
-
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
-    html = response.read()
-    print('Body response:')
-    response = {'type': type(html), 'content': html,
-                'utf8 content': html.decode('utf-8')}
-    [print('\t- {}: {}'.format(k, v)) for k, v in response.items()]
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        response = {'type': type(html), 'content': html,
+                    'utf8 content': html.decode('utf-8')}
+        [print('\t- {}: {}'.format(k, v)) for k, v in response.items()]
