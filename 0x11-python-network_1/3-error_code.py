a8c26413102f4e391e01f3c7b460cc397f648436
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-09 14:04:25 -0500

    Add 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,11 +2,12 @@
 """ This module consider an error in the request """
 import urllib.request
 import urllib.error
+import sys
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+        with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read())
 
     except urllib.error.URLError as e:
-        print(e.reason)
+        print('Error Code: {}'.format(e.code))
