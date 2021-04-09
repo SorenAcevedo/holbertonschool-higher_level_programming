d2f5c20275dbbdb7793548f78e6fdbe49c137953
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-09 15:10:29 -0500

    Change to post method

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -4,5 +4,6 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    r = requests.post(sys.argv[1], data={'email': sys.argv[2]})
+    values = {'email': sys.argv[2]}
+    r = requests.post(sys.argv[1], data=values)
     print(r.text)
