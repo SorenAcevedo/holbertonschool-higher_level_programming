73a8b9e647dc80aa4ef1205810670f39504220f4
SorenAcevedo <sorenacevedo@gmail.com>
2021-04-09 11:57:12 -0500

    Add 0, body response

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" This module fetches url """
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    html = response.read()
+    print('Body response:')
+    response = {'type': type(html), 'content': html,
+                'utf8 content': html.decode()}
+    [print('    - {}: {}'.format(k, v)) for k, v in response.items()]
