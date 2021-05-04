f34ee1a58cbd5a2a11f1181ed349bc977d77a41f
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-04 14:24:44 -0500

    Change request

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,15 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/18/';
+let cont = 0;
+
 request(process.argv[2], function (error, response) {
   if (error) {
     return console.log(error);
   }
   const json = JSON.parse(response.body).results;
-  let cont = 0;
   for (let i = 0; json[i]; i++) {
     for (let j = 0; json[i].characters[j]; j++) {
-      if (json[i].characters[j] === url) { cont++; }
+      if (json[i].characters[j].endsWith('18/')) { cont++; }
     }
   }
   console.log(cont);
