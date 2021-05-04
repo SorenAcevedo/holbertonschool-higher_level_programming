37e1323f959f0028af7ac908e03568a659625eb6
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-04 13:02:07 -0500

    Add 6 beuty challenge

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,11 +1,18 @@
 #!/usr/bin/node
 const request = require('request');
 const url = process.argv[2];
-let output_obj = {};
+const outputObj = {};
 request(url, function (error, response) {
   if (error) {
     return console.log(error);
   }
   const json = JSON.parse(response.body);
-  console.log(json);
+  for (let i = 0; json[i]; i++) {
+    if (outputObj[json[i].userId]) {
+      if (json[i].completed === true) { outputObj[json[i].userId] += 1; }
+    } else {
+      if (json[i].completed === true) { outputObj[json[i].userId] = 1; }
+    }
+  }
+  console.log(outputObj);
 });
