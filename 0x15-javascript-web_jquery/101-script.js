a7837f65acc6343225d7af9b9b436e65624a6b16
SorenAcevedo <sorenacevedo@gmail.com>
2021-05-12 12:03:23 -0500

    Add 2 advanced tasks

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,1 +1,12 @@
-#!/usr/bin/node
+$(function () {
+  $('#add_item').click(function () {
+    const item = $('<li></li>').text('Item');
+    $('.my_list').append(item);
+  });
+	$('#remove_item').click(function () {
+    $('li:last-child').remove();
+  });
+	$('#clear_list').click(function () {
+    $('li').remove();
+  });
+});
